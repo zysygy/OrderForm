b7f2dc76943a96c7e085f9635c6e9c4b2a2bcf56
zysygy <allensaccount@yahoo.ca>
2015-02-12 21:18:20 -0500

    Initial Pagination Functionality Implemented
    Rough scripts for dynamically adding new pages has been
    added. Margins and some styles have been modded to
    accomodate changes. Current scripts have testing code
    built in.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A web app to create orders for a business
 Requirements:
 
 * Produce order form layout as per existing physical form
-* Generate unique order ID for each form
+* Generate order ID with minimal collisions for each forms
 * Work offline
 * Compatible with iOS Devices
 * Print to AirPrint device
--- js/scripts.js
+++ js/scripts.js
@@ -25,10 +25,76 @@ function genOrdNo () {
 	return (randSegment()+ randSegment() + "-" + randSegment() + randSegment() + randSegment() + "-" + randSegment());
 }
 
+function newPage (){
+	//Could play with clone and append, but going to hard code for now
+	var ret;
+	var insert = '<div class="page-wrap">\
+		<div class="order-details">\
+		<table>\
+		<tr id="colour-labels">\
+			<th class="style">Style</th>\
+			<th>Black</th>\
+			<th>Brown</th>\
+			<th>White</th>\
+			<th>Gray</th>\
+			<th>Blue</th>\
+			<th>Beige</th>\
+			<th>Red</th>\
+			<th>Purp.</th>\
+			<th>Gold</th>\
+			<th>Silver</th>\
+			<th>Green</th>\
+			<th>Burg.</th>\
+			<th>Pink</th>\
+			<th>Yel.</th>\
+			<th>Oran.</th>\
+			<th class="qty">Qty</th>\
+			<th class="price">Price</th>\
+		</tr>\
+		<tr class="item-row">\
+			<td class="style">\
+				<div class="del-wrap">\
+					<textarea maxlength="6">123456</textarea>\
+					<a class="del-item" href="javascript:;" title="Remove Item">X</a>\
+				</div>\
+			</td>\
+			<td><textarea class="item" maxlength="3">1</textarea></td>\
+			<td><textarea class="item" maxlength="3">1</textarea></td>\
+			<td><textarea class="item" maxlength="3">1</textarea></td>\
+			<td><textarea class="item" maxlength="3">1</textarea></td>\
+			<td><textarea class="item" maxlength="3">1</textarea></td>\
+			<td><textarea class="item" maxlength="3">1</textarea></td>\
+			<td><textarea class="item" maxlength="3">1</textarea></td>\
+			<td><textarea class="item" maxlength="3">1</textarea></td>\
+			<td><textarea class="item" maxlength="3">1</textarea></td>\
+			<td><textarea class="item" maxlength="3">1</textarea></td>\
+			<td><textarea class="item" maxlength="3">1</textarea></td>\
+			<td><textarea class="item" maxlength="3">1</textarea></td>\
+			<td><textarea class="item" maxlength="3">1</textarea></td>\
+			<td><textarea class="item" maxlength="3">1</textarea></td>\
+			<td><textarea class="item" maxlength="3">1</textarea></td>\
+			<td class="qty"><textarea maxlength="3">999</textarea></td>\
+			<td class="price"><textarea maxlength="6">999.99</textarea></td>\
+		</tr>\
+		<tr>\
+			<td id="add-row" colspan="18"><a id="add-item" href="javascript:;" title="Add item">Add Item</a></td>\
+		</tr>\
+		</table></div></div>';
+	
+	if ($(".item-row").length == 29 && $(".page-wrap").length < 2){
+		//delete add button, insert table header and new row, order no
+		$("#add-row").parents("tr").remove();
+		$("#CC").after(insert);
+	}
+	/*else if (){ //item-row.length - 29 
+	
+	}*/
+}	
+
 $(document).ready(function() {
 	$(".del-item").hide(); //Hide initial delete button to prevent accidental use
 	
-	$("#add-item").click(function(){
+	$( document ).on("click","#add-item", function(){
 		var insert = '<tr class="item-row"><td class="style">\
 		<div class="del-wrap">\
 		<textarea maxlength="6">123456</textarea>\
@@ -51,7 +117,13 @@ $(document).ready(function() {
 		<td><textarea class="item" maxlength="3">1</textarea></td>\
 		<td class="qty"><textarea maxlength="3">999</textarea></td>\
 		<td class="price"><textarea maxlength="6">999.99</textarea></td></tr>';
-		$(".item-row:last").after(insert);
+		newPage();
+		for (i=1; $(".item-row").length < 29; i++){
+			$(".item-row:last").after(insert);
+		}
+		if ($(".item-row").length > 30){
+			$(".item-row:last").after(insert);
+		}
 		if ($(".del-item").length > 1) $(".del-item").show();
 	});
 	
@@ -60,8 +132,10 @@ $(document).ready(function() {
 		if ($(".del-item").length < 2) $(".del-item").hide();
 	});
 	
+	//if there are >29 item rows, insert new page
+	
 	$("#customer .date-field").val(print_today());
 	
-	$("#ord-no").val(genOrdNo());
+	$("#order-num .field").val(genOrdNo());
 });
 
